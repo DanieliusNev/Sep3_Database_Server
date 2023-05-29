2ab95a904f245d0a703b5605d7f0bfce601778ac
DanieliusNev <danieliusneverdauskas@gmail.com>
2023-05-29 15:21:19 +0200

    Update sockets

--- src/main/java/handlers/ShareHandler.java
+++ src/main/java/handlers/ShareHandler.java
@@ -9,6 +9,8 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
 public class ShareHandler {
     private Connection connection;
 
@@ -19,19 +21,21 @@ public class ShareHandler {
     public String handleRequest(String action, JsonNode requestDataNode) {
         switch (action) {
             case "shareExercises":
-                int userId = requestDataNode.get("userId").asInt();
+                String userIdString = requestDataNode.get("sharedBy").asText();
+                int userId = parseInt(userIdString);
+                System.out.println(userId);
                 List<Integer> exerciseIds = extractExerciseIds(requestDataNode);
                 String comment = requestDataNode.get("comment").asText();
                 return shareExercises(userId, exerciseIds, comment);
             case "getSharedPostsByUser":
                 System.out.println("reached case");
-                return getSharedPostsByUser();
+                return getSharedPosts();
             default:
                 return "Invalid action";
         }
     }
 
-    public String shareExercises(int userId, List<Integer> exerciseIds, String comment) {
+    /*public String shareExercises(int userId, List<Integer> exerciseIds, String comment) {
         String sharedPostsQuery = "INSERT INTO shared_posts (shared_by, comment) VALUES (?, ?)";
         String sharedPostExercisesQuery = "INSERT INTO shared_post_exercises (shared_post_id, exercise_id) VALUES (?, ?)";
 
@@ -57,19 +61,61 @@ public class ShareHandler {
         } catch (SQLException e) {
             throw new RuntimeException("Failed to share exercises", e);
         }
+    }*/
+    public String shareExercises(int userId, List<Integer> exerciseIds, String comment) {
+        String sharedPostsQuery = "INSERT INTO shared_posts (shared_by, comment) VALUES (?, ?)";
+        String sharedPostExercisesQuery = "INSERT INTO shared_post_exercises (shared_post_id, exercise_id) VALUES (?, ?)";
+
+        try (PreparedStatement sharedPostsStatement = connection.prepareStatement(sharedPostsQuery, PreparedStatement.RETURN_GENERATED_KEYS);
+             PreparedStatement sharedPostExercisesStatement = connection.prepareStatement(sharedPostExercisesQuery)) {
+
+            sharedPostsStatement.setInt(1, userId);
+            sharedPostsStatement.setString(2, comment);
+            sharedPostsStatement.executeUpdate();
+
+            ResultSet generatedKeys = sharedPostsStatement.getGeneratedKeys();
+            int sharedPostId;
+            if (generatedKeys.next()) {
+                sharedPostId = generatedKeys.getInt(1);
+                System.out.println("postid generated one: " + sharedPostId);
+                for (int exerciseId : exerciseIds) {
+                    sharedPostExercisesStatement.setInt(1, sharedPostId);
+                    System.out.println("postid generated2: " + sharedPostId);
+                    sharedPostExercisesStatement.setInt(2, exerciseId);
+                    System.out.println("exID: " + exerciseId);
+                    sharedPostExercisesStatement.executeUpdate();
+                }
+            } else {
+                throw new SQLException("Failed to retrieve generated keys for shared_posts");
+            }
+
+            return "Exercises shared successfully";
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to share exercises", e);
+        }
     }
 
-    public String getSharedPostsByUser() {
+
+
+
+    public String getSharedPosts() {
         List<SharedPost> sharedPosts = new ArrayList<>();
+        /*String query = "SELECT sp.id AS shared_post_id, sp.shared_date, sp.comment, u.username, e.title " +
+                "FROM shared_posts sp " +
+                "JOIN user_profile u ON sp.shared_by = u.id " +
+                "JOIN shared_post_exercises sed ON sp.id = sed.shared_post_id " +
+                "JOIN exercises e ON sed.exercise_id = e.id " +
+                "ORDER BY sp.shared_date DESC";*/
         String query = "SELECT sp.id AS shared_post_id, sp.shared_date, sp.comment, u.username, e.title " +
                 "FROM shared_posts sp " +
                 "JOIN user_profile u ON sp.shared_by = u.id " +
                 "JOIN shared_post_exercises sed ON sp.id = sed.shared_post_id " +
                 "JOIN exercises e ON sed.exercise_id = e.id " +
                 "ORDER BY sp.shared_date DESC";
-        System.out.println(query);
+
         try (PreparedStatement statement = connection.prepareStatement(query);
              ResultSet resultSet = statement.executeQuery()) {
+
             while (resultSet.next()) {
                 int sharedPostId = resultSet.getInt("shared_post_id");
                 String sharedDate = resultSet.getString("shared_date");
@@ -101,7 +147,7 @@ public class ShareHandler {
 
     private List<Integer> extractExerciseIds(JsonNode requestDataNode) {
         List<Integer> exerciseIds = new ArrayList<>();
-        JsonNode exercisesNode = requestDataNode.get("exercises");
+        JsonNode exercisesNode = requestDataNode.get("exerciseTitles");
         if (exercisesNode != null && exercisesNode.isArray()) {
             for (JsonNode exerciseNode : exercisesNode) {
                 int exerciseId = exerciseNode.get("exerciseId").asInt();
